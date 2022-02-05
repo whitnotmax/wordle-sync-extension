503399b7caad1d4e4480cd027ec0f6c75d172372
whitnotmax <91440193+whitnotmax@users.noreply.github.com>
2022-02-05 13:23:27 -0500

    more clear behavior

--- src/background.js
+++ src/background.js
@@ -44,6 +44,7 @@ chrome.runtime.onMessage.addListener(
               console.log("data is: " + docSnap.data().data);
               chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
                 chrome.tabs.sendMessage(tabs[0].id, {"reason": "data", "data": docSnap.data().data}, function(response) {});
+
               });
             } else {
               chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
@@ -65,6 +66,9 @@ chrome.runtime.onMessage.addListener(
           console.log(request.data);
           const docRef = doc(db, "users", auth.currentUser.uid);
           setDoc(docRef, {data: request.data});
+          chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {"reason": "reload"}, function(response) {});
+          });
         }
 
         sendMessage(true);
--- src/injection.js
+++ src/injection.js
@@ -2,16 +2,17 @@ chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     switch (request.reason) {
       case "data":
+        const notification = document.createElement("p");
+        notification.innerText = "You have never synced your data to the Wordle Sync extension! Your stats may be inaccurate until you do."
+        notification.style.color = "#ff0000";
+        document.body.prepend(notification);
         console.log("data from firestore: " + request.data);
         if (request.data === "unauthenticated") {
-          localStorage.statistics = "";
-          const notification = document.createElement("p");
-          notification.innerText = "You are not signed in to Wordle Sync extension! Your stats may not be available until you sign in."
-          notification.style.color = "#ff0000";
-          document.body.prepend(notification);
+          notification.innerText = "You are not signed in to Wordle Sync extension! Your stats may be unavailable or inaccurate until you sign in."
         } else {
           if (request.data !== "") {
             localStorage.statistics = request.data;
+            notification.remove();
           } else {
             console.log("Not updating with blank data!");
           }
@@ -26,6 +27,7 @@ chrome.runtime.onMessage.addListener(
       
       case "reload":
         // for when we login or signup
+        console.log("reload request");
         location.reload();
         sendResponse(true);
       default: 
